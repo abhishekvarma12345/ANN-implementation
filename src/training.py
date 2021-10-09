e75f836e2583faa125e7959bef6cd00c866cb53a
abhishekvarma12345 <abhishekvarmad@gmail.com>
2021-10-09 18:35:43 +0530

    os module not imported resolved

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model
+import os
 import argparse
 
 def training(config_path):
